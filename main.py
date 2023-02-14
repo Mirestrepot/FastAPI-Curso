8ecd404df62a96deaec65fcfb233ca8506024174
Mirestrepot <mirestrepot@unal.edu.co>
2023-02-14 11:25:40 -0500

    Validation and Path

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ from fastapi import Body,Query,Path
 app = FastAPI()
 
 #Models
+class Location(BaseModel):
+    city: str
+    state: str
+    country: str
 
 class Person(BaseModel):
     first_name : str
@@ -56,3 +60,18 @@ def show_person(
 	person_id: int = Path(ge=0)
 ):
 	return {person_id: "It exist!"}
+
+#Validation: Request Body
+@app.put("/person/{person_id}")
+def update_person(
+    person_id: int = Path(
+        ...,title="Person ID",
+        description="This is the person ID",
+        gt=0
+    ),
+    person: Person = Body(...),
+    location: Location = Body(...)
+): 
+    results = person.dict()
+    results.update((location.dict()))
+    return  results
