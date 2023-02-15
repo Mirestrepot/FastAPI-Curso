ad0d2e796fdb91790bd3cda068e1964f36067fb3
Mirestrepot <mirestrepot@unal.edu.co>
2023-02-14 19:07:03 -0500

    add header and cookies

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ from enum import Enum
 #Pydantic
 from pydantic import BaseModel
 from pydantic import Field
-
+from pydantic import EmailStr
 
 #FastAPI
 from fastapi import FastAPI
-from fastapi import Body,Query,Path,Form
+from fastapi import Body,Query,Path,Form,Header,Cookie
 from fastapi import status
 
 app = FastAPI()
@@ -142,7 +142,7 @@ def show_person(
         description="This is the person name. It's betwwen 1 and 50 characters",
         example="Rocio"
         ),
-    age :   Optional[str] =Query(
+    age :   Optional[str] = Query(
         default=None,
         min_length=1,
         max_length=20,
@@ -162,9 +162,9 @@ def show_person(
 
 def show_person(
 	person_id: int = Path(
-    ...,
-    ge=0,
-    example=123
+        ...,
+        ge=0,
+        example=123
     )
 ):
 	return {person_id: "It exist!"}
@@ -190,6 +190,7 @@ def update_person(
     results.update((location.dict()))
     return  results
 
+#Forms
 @app.post(
     path="/login",
     response_model=LoginOut,
@@ -201,3 +202,27 @@ def login(
 ):
     return LoginOut(username=username)
     
+#Cookies and Headers Parameters
+@app.post(
+    path="/contact",
+    status_code=status.HTTP_200_OK
+)
+def contact(
+    first_name: str = Form(
+        ...,
+        max_length=20,
+        min_length=1     
+    ),
+    last_name: str = Form(
+        ...,
+        max_length=20,
+        min_length=1
+    ),
+    email: EmailStr = Form(...),
+        message: str = Form(
+        ...,
+        min_length=20 
+    ),
+    user_agent: Optional[str] = Header(default=None),
+    ads: Optional[str] = Cookie(default=None)
+):return user_agent
